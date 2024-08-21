d63c8c43a06e41f6f222d8a93d8f95d6460efd8b
umair rasheed <umairg404@gmail.com>
2024-08-21 16:06:48 +0500

    Add initial setup for .NET 8.0 app with Serilog and Swagger
    Updated .dockerignore to exclude unnecessary files and directories.
    Created/updated DotNetCoreSerilogPOC.csproj to target .NET 8.0, enable nullable reference types, and add necessary package references.
    Added DotNetCoreSerilogPOC.http to define a GET request to /weatherforecast endpoint.
    Created DotNetCoreSerilogPOC.sln for Visual Studio.
    Set up basic ASP.NET Core app in Program.cs with Serilog and Swagger.
    Added launchSettings.json to configure different profiles for running the app.
    Added appsettings.Development.json to configure logging levels for development.
    Added appsettings.json to configure logging levels and Serilog settings.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseSerilogRequestLogging();
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", (ILogger<Program> logger) =>
+{
+    // Log the beginning of the request
+    logger.LogInformation("Generating weather forecast.");
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    // Log the generated forecast
+    logger.LogInformation("Generated forecast: {@Forecast}", forecast);
+
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
